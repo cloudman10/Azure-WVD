1dd3102867d2d6d23e81281db011c5925d67e473
Sabih Saeed <sabih.saeed@microsoft.com>
2020-08-09 15:14:05 +1000

    Update New-WVDSessionHost.ps1

--- PowerShell/New-WVDSessionHost.ps1
+++ PowerShell/New-WVDSessionHost.ps1
@@ -1,26 +1,3 @@
-<#Author       : Dean Cefola
-# Creation Date: 09-15-2019
-# Usage        : Windows Virtual Desktop Scripted Install
-
-#********************************************************************************
-# Date                         Version      Changes
-#------------------------------------------------------------------------
-# 09/15/2019                     1.0        Intial Version
-# 09/16/2019                     2.2        Add Input Parameters 
-# 09/16/2019                     2.3        Add TLS 1.2 settings
-# 09/17/2019                     3.0        Chang download locations to dynamic
-# 09/17/2019                     3.1        Add code to disable IESEC for admins
-# 09/20/2019                     3.2        Add code to discover OS (Server / Client)
-# 09/20/2019                     4.0        Add code for servers to add RDS Host role
-# 10/01/2019                     4.2        Add all FSLogix Profile Container Reg entries for easier management
-# 10/07/2019                     4.3        Add FSLogix Office Container Reg entries for easier management
-# 10/16/2019                     5.0        Add Windows 7 Support
-# 07/20/2020                     6.0        Add WVD Optimize Code from The-Virtual-Desktop-Team
-#
-#*********************************************************************************
-#
-#>
-
 
 ##############################
 #    WVD Script Parameters   #
@@ -39,17 +16,8 @@ Param (
 $LocalWVDpath            = "c:\temp\wvd\"
 $WVDBootURI              = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
 $WVDAgentURI             = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
-
-
 $WVDAgentInstaller       = 'WVD-Agent.msi'
 $WVDBootInstaller        = 'WVD-Bootloader.msi'
-$Win7x64_UpdateURI       = 'https://download.microsoft.com/download/A/F/5/AF5C565C-9771-4BFB-973B-4094C1F58646/Windows6.1-KB2592687-x64.msu'                                        
-$Win7x64_WMI5URI         = 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip'
-$Win7x64_UpdateInstaller = 'Win7-KB2592687-x64.msu'
-$Win7x64_WMI5Installer   = 'Win7-KB3191566-WMI5-x64.zip'
-$Win7x64_WVDAgent        = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE3JZCm'
-$Win7x64_WVDBootMgrURI   = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE3K2e3'
-
 
 ####################################
 #    Test/Create Temp Directory    #
@@ -84,14 +52,13 @@ else {
         -BackgroundColor Black `
         "c:\temp\wvd directory already exists"
 }
-New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+
 Add-Content `
 -LiteralPath C:\New-WVDSessionHost.log `
 "
 RegistrationToken = $RegistrationToken
 Optimize          = $Optimize
 "
-
 
 #################################
 #    Download WVD Componants    #
@@ -101,91 +68,14 @@ Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Boot Loader"
 Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading WVD Agent"
     Invoke-WebRequest -Uri $WVDAgentURI -OutFile "$LocalWVDpath$WVDAgentInstaller"
 
-
-##############################
-#    OS Specific Settings    #
-##############################
-$OS = (Get-WmiObject win32_operatingsystem).name
-If(($OS) -match 'server') {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows Server OS Detected"
-    write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Server OS Detected"
-    If(((Get-WindowsFeature -Name RDS-RD-Server).installstate) -eq 'Installed') {
-        "Session Host Role is already installed"
-    }
-    Else {
-        "Installing Session Host Role"
-        Install-WindowsFeature `
-            -Name RDS-RD-Server `
-            -Verbose `
-            -LogPath "$LocalWVDpath\RdsServerRoleInstall.txt"
-    }
-    $AdminsKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UsersKey = "SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    $BaseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey("LocalMachine","Default")
-    $SubKey = $BaseKey.OpenSubkey($AdminsKey,$true)
-    $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
-    $SubKey = $BaseKey.OpenSubKey($UsersKey,$true)
-    $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)    
-}
-Else {
-    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows Client OS Detected"
-    write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Client OS Detected"
-    if(($OS) -match 'Windows 10') {
-        write-host `
-            -ForegroundColor Yellow `
-            -BackgroundColor Black  `
-            "Windows 10 detected...skipping to next step"
-        Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows 10 Detected...skipping to next step"     
-    }    
-    else {
-        $OSArch = (Get-WmiObject win32_operatingsystem).OSArchitecture
-        If(($OSArch) -match '64-bit') {
-            write-host `
-                -ForegroundColor Magenta  `
-                -BackgroundColor Black `
-                "Windows 7 x64 detected"
-            Add-Content -LiteralPath C:\New-WVDSessionHost.log "Windows 7 x64 Detected"
-
-
-            #################################
-            #    Begin Win7x64 downloads    #
-            #################################
-            $Win7x64_WinUpdateRequest = [System.Net.WebRequest]::Create($Win7x64_UpdateURI)
-            $Win7x64_WMI5Request      = [System.Net.WebRequest]::Create($Win7x64_WMI5URI)            
-            $Win7x64_WVDAgentRequest  = [System.Net.WebRequest]::Create($Win7x64_WVDAgentURI)
-            $Win7x64_WVDBootRequest   = [System.Net.WebRequest]::Create($Win7x64_WVDBootMgrURI)
-
-
-            ################################
-            #    Begin Win7x64 Installs    #
-            ################################
-            write-host `
-                -ForegroundColor Magenta `
-                -BackgroundColor Black `
-                "...installing Update KB2592687 for x64"
-            Expand-Archive `
-                -LiteralPath "C:\temp\wvd\$Win7x64_WMI5Installer" `
-                -DestinationPath "$LocalWVDpath\Win7Wmi5x64" `
-                -Force `
-                -Verbose
-            $packageName = 'Win7AndW2K8R2-KB3191566-x64.msu'
-            $packagePath = 'C:\temp\wvd\Win7Wmi5x64'
-            $wusaExe = "$env:windir\system32\wusa.exe"
-            $wusaParameters += @("/quiet", "/promptrestart")
-            $wusaParameterString = $wusaParameters -join " "
-            & $wusaExe $wusaParameterString
-        }        
-    }
-}
-
-
 ################################
 #    Install WVD Componants    #
 ################################
+Set-Location -Path $LocalWVDpath
 Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Bootloader"
 $bootloader_deploy_status = Start-Process `
     -FilePath "msiexec.exe" `
-    -ArgumentList "/i $WVDBootInstaller", `
+    -ArgumentList "/i $LocalWVDpath$WVDBootInstaller", `
         "/quiet", `
         "/qn", `
         "/norestart", `
@@ -201,7 +91,7 @@ Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing WVD Agent"
 Write-Output "Installing RD Infra Agent on VM $AgentInstaller`n"
 $agent_deploy_status = Start-Process `
     -FilePath "msiexec.exe" `
-    -ArgumentList "/i $WVDAgentInstaller", `
+    -ArgumentList "/i $LocalWVDpath$WVDAgentInstaller", `
         "/quiet", `
         "/qn", `
         "/norestart", `
